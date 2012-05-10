5e4d0490f4323f0c90fa5891e2486b5e2fe753e7
Colin Hebert <colin.hebert@oucs.ox.ac.uk>
2012-05-10 17:39:30 +0100

    Create a searchFilter based on security access

---
+++ impl/src/main/java/uk/ac/ox/oucs/search/solr/filter/SecuritySearchFilter.java
@@ -0,0 +1,124 @@
+package uk.ac.ox.oucs.search.solr.filter;
+
+import org.sakaiproject.search.api.EntityContentProducer;
+import org.sakaiproject.search.api.SearchIndexBuilder;
+import org.sakaiproject.search.api.SearchResult;
+import org.sakaiproject.search.api.TermFrequency;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
+public class SecuritySearchFilter implements SearchItemFilter {
+    private final SearchIndexBuilder searchIndexBuilder;
+    public static final SearchResult censoredSearchResult = new CensoredSearchResult();
+
+    public SecuritySearchFilter(SearchIndexBuilder searchIndexBuilder) {
+        this.searchIndexBuilder = searchIndexBuilder;
+    }
+
+    @Override
+    public SearchResult filter(SearchResult result) {
+        String reference = result.getReference();
+        EntityContentProducer ecp = searchIndexBuilder.newEntityContentProducer(reference);
+        return ecp == null || !ecp.canRead(reference) ? censoredSearchResult : result;
+    }
+
+
+    private static class CensoredSearchResult implements SearchResult {
+        private static final TermFrequency termFrequency = new TermFrequency() {
+            @Override
+            public String[] getTerms() {
+                return new String[0];
+            }
+
+            @Override
+            public int[] getFrequencies() {
+                return new int[0];
+            }
+        };
+
+        @Override
+        public float getScore() {
+            return 0;
+        }
+
+        @Override
+        public String getId() {
+            return "";
+        }
+
+        @Override
+        public String[] getFieldNames() {
+            return new String[0];
+        }
+
+        @Override
+        public String[] getValues(String string) {
+            return new String[0];
+        }
+
+        @Override
+        public Map<String, String[]> getValueMap() {
+            return Collections.emptyMap();
+        }
+
+        @Override
+        public String getUrl() {
+            return "";
+        }
+
+        @Override
+        public String getTitle() {
+            return "";
+        }
+
+        @Override
+        public int getIndex() {
+            return 0;
+        }
+
+        @Override
+        public String getSearchResult() {
+            return "";
+        }
+
+        @Override
+        public String getReference() {
+            return "";
+        }
+
+        @Override
+        public TermFrequency getTerms() throws IOException {
+            return termFrequency;
+        }
+
+        @Override
+        public String getTool() {
+            return "";
+        }
+
+        @Override
+        public boolean isCensored() {
+            return true;
+        }
+
+        @Override
+        public String getSiteId() {
+            return "";
+        }
+
+        @Override
+        public void toXMLString(StringBuilder sb) {
+        }
+
+        @Override
+        public void setUrl(String newUrl) {
+        }
+
+        @Override
+        public boolean hasPortalUrl() {
+            return false;
+        }
+    }
+}
