e499834784e968f5c9fc67237182107980b2c3f6
Colin Hebert <colin.hebert@oucs.ox.ac.uk>
2012-05-18 13:54:31 +0100

    Create a contentProducer for ReadingList citation

---
+++ impl/src/main/java/uk/ac/ox/oucs/search/solr/producer/CitationContentProducer.java
@@ -0,0 +1,57 @@
+package uk.ac.ox.oucs.search.solr.producer;
+
+import org.sakaiproject.citation.api.Citation;
+import org.sakaiproject.citation.api.CitationCollection;
+import org.sakaiproject.citation.api.CitationService;
+
+import java.io.Reader;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Colin Hebert
+ */
+public class CitationContentProducer extends ContentHostingContentProducer {
+    private CitationService citationService;
+
+    @Override
+    protected boolean isContentTypeSupported(String contentType) {
+        return CitationService.CITATION_LIST_ID.equals(contentType);
+    }
+
+    @Override
+    public boolean isContentFromReader(String reference) {
+        return false;
+    }
+
+    @Override
+    public Reader getContentReader(String reference) {
+        return null;
+    }
+
+    @Override
+    public String getContent(String reference) {
+        try {
+            String citationCollectionId  = new String(contentHostingService.getResource(getId(reference)).getContent());
+            CitationCollection collection = citationService.getCollection(citationCollectionId);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (Citation citation : (List<Citation>) collection.getCitations()) {
+                sb.append(citation.getId()).append('\n')
+                        .append(citation.getSchema().getIdentifier()).append('\n');
+
+                for (Map.Entry<String, String> property : ((Map<String, String>) citation.getCitationProperties()).entrySet()) {
+                    sb.append(property.getKey()).append(':').append(property.getValue()).append('\n');
+                }
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setCitationService(CitationService citationService) {
+        this.citationService = citationService;
+    }
+}
