180ab8376f1a24c54cd72e2559b21c29aff09e04
Colin Hebert <colin.hebert@it.ox.ac.uk>
2012-11-16 10:54:47 +0000

    Document SolrProcess

--- impl/src/main/java/uk/ac/ox/oucs/search/solr/process/SolrProcess.java
+++ impl/src/main/java/uk/ac/ox/oucs/search/solr/process/SolrProcess.java
@@ -1,6 +1,9 @@
 package uk.ac.ox.oucs.search.solr.process;
 
 /**
+ * Process executing some Solr operations.
+ * This process could be stored in a queuing system, its content should be as limited as possible (to avoid memory leaks)
+ *
  * @author Colin Hebert
  */
 public interface SolrProcess {
