c0e69606f14aa0476f9393538276f5b6d44f5a30
Colin Hebert <colin.hebert@it.ox.ac.uk>
2012-12-03 13:26:07 +0000

    Revert "Remove the solrServerFactory and use the only instance of the server instead"
    This reverts commit 95ef53bf9a45d94cf832e4eb1558a79f8a91a997.

--- impl/src/main/java/uk/ac/ox/oucs/search/solr/indexing/SolrTaskHandler.java
+++ impl/src/main/java/uk/ac/ox/oucs/search/solr/indexing/SolrTaskHandler.java
@@ -9,6 +9,7 @@ import org.sakaiproject.search.api.SearchService;
 import org.sakaiproject.thread_local.api.ThreadLocalManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.ObjectFactory;
 import uk.ac.ox.oucs.search.indexing.Task;
 import uk.ac.ox.oucs.search.indexing.TaskHandler;
 import uk.ac.ox.oucs.search.indexing.exception.NestedTaskHandlingException;
@@ -30,7 +31,7 @@ import static uk.ac.ox.oucs.search.solr.indexing.SolrTask.Type.*;
 public class SolrTaskHandler implements TaskHandler {
     private static final Logger logger = LoggerFactory.getLogger(SolrTaskHandler.class);
     private ContentProducerFactory contentProducerFactory;
-    private SolrServer solrServer;
+    private ObjectFactory solrServerFactory;
     private SolrTools solrTools;
 
     @Override
@@ -38,6 +39,7 @@ public class SolrTaskHandler implements TaskHandler {
         logger.debug("Attempt to handle '" + task + "'");
         try {
             String taskType = task.getType();
+            SolrServer solrServer = (SolrServer) solrServerFactory.getObject();
             try {
                 if (INDEX_DOCUMENT.getTypeName().equals(taskType)) {
                     indexDocument(task.getProperty(DefaultTask.RESOURCE_NAME), task.getCreationDate(), solrServer);
@@ -253,8 +255,8 @@ public class SolrTaskHandler implements TaskHandler {
         this.contentProducerFactory = contentProducerFactory;
     }
 
-    public void setSolrServer(SolrServer solrServer) {
-        this.solrServer = solrServer;
+    public void setSolrServerFactory(ObjectFactory solrServerFactory) {
+        this.solrServerFactory = solrServerFactory;
     }
 
     public void setSolrTools(SolrTools solrTools) {
