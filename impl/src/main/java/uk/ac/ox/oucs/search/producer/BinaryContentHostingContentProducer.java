c2a62dca78c861bc97b58eb9921d0ed95acbc629
Colin Hebert <colin.hebert@it.ox.ac.uk>
2012-12-05 17:47:38 +0000

    Add a maximum size for handled binary files (set to 30MB in the config)

--- impl/src/main/java/uk/ac/ox/oucs/search/producer/BinaryContentHostingContentProducer.java
+++ impl/src/main/java/uk/ac/ox/oucs/search/producer/BinaryContentHostingContentProducer.java
@@ -2,7 +2,10 @@ package uk.ac.ox.oucs.search.producer;
 
 import org.sakaiproject.content.api.ContentResource;
 import org.sakaiproject.search.api.StoredDigestContentProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.Reader;
 import java.util.List;
@@ -11,7 +14,10 @@ import java.util.List;
  * @author Colin Hebert
  */
 public class BinaryContentHostingContentProducer extends ContentHostingContentProducer implements BinaryEntityContentProducer, StoredDigestContentProducer {
+    private static final Logger logger = LoggerFactory.getLogger(BinaryContentHostingContentProducer.class);
+    private static final byte[] EMPTY_DOCUMENT = new byte[0];
     private List<String> supportedContentTypes;
+    private long documentMaximumSize = Long.MAX_VALUE;
 
     @Override
     public boolean isContentFromReader(String reference) {
@@ -37,20 +43,24 @@ public class BinaryContentHostingContentProducer extends ContentHostingContentPr
         ContentResource contentResource;
         try {
             contentResource = contentHostingService.getResource(getId(ref));
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to resolve resource " + ref, e);
-        }
 
-        InputStream stream;
-        try {
-            stream = contentResource.streamContent();
+            if (contentResource.getContentLength() > documentMaximumSize) {
+                logger.info("The document '"+ref+"' was bigger ("+contentResource.getContentLength()+"B)" +
+                        "than the maximum expected size ("+documentMaximumSize+"B), its content won't be handled");
+                return new ByteArrayInputStream(EMPTY_DOCUMENT);
+            } else {
+                return contentResource.streamContent();
+            }
         } catch (Exception ex) {
             throw new RuntimeException("Failed to obtain content from " + ref, ex);
         }
-        return stream;
     }
 
     public void setSupportedContentTypes(List<String> supportedContentTypes) {
         this.supportedContentTypes = supportedContentTypes;
+    }
+
+    public void setDocumentMaximumSize(long documentMaximumSize) {
+        this.documentMaximumSize = documentMaximumSize;
     }
 }
