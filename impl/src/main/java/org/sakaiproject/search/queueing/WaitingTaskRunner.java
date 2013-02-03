9056144d0b971e194d539fd7a23df1d4e5a509ce
Colin Hebert <hebert.colin@gmail.com>
2013-02-03 10:49:12 +0100

    Reduce visibility when public isn't necessary

--- impl/src/main/java/org/sakaiproject/search/queueing/WaitingTaskRunner.java
+++ impl/src/main/java/org/sakaiproject/search/queueing/WaitingTaskRunner.java
@@ -29,7 +29,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author Colin Hebert
  */
 public abstract class WaitingTaskRunner implements TaskRunner {
-    public static final int BASE_WAITING_TIME = 1000;
+    private static final int BASE_WAITING_TIME = 1000;
     private static final Logger logger = LoggerFactory.getLogger(WaitingTaskRunner.class);
     private final ReentrantLock taskRunnerLock = new ReentrantLock();
     /**
