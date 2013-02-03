52ba8f30a7381dcc894f4fb7a47175563968a178
Colin Hebert <hebert.colin@gmail.com>
2013-02-03 11:21:51 +0100

    Add documentation to filters

--- impl/src/main/java/org/sakaiproject/search/response/filter/SearchItemFilter.java
+++ impl/src/main/java/org/sakaiproject/search/response/filter/SearchItemFilter.java
@@ -2,6 +2,17 @@ package org.sakaiproject.search.response.filter;
 
 import org.sakaiproject.search.api.SearchResult;
 
+/**
+ * Filter for search results, allowing to massage the returned result.
+ *
+ * @author Colin Hebert
+ */
 public interface SearchItemFilter {
+    /**
+     * Filters a result.
+     *
+     * @param result original result.
+     * @return massaged result.
+     */
     SearchResult filter(SearchResult result);
 }
