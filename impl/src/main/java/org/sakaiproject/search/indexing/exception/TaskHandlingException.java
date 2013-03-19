40b4aafaf27af3735482b4b852546861448d1985
Colin Hebert <colin.hebert@it.ox.ac.uk>
2013-03-19 17:38:22 +0000

    Add documentation to exceptions

--- impl/src/main/java/org/sakaiproject/search/indexing/exception/TaskHandlingException.java
+++ impl/src/main/java/org/sakaiproject/search/indexing/exception/TaskHandlingException.java
@@ -9,17 +9,44 @@ package org.sakaiproject.search.indexing.exception;
  * @author Colin Hebert
  */
 public class TaskHandlingException extends RuntimeException {
+    /**
+     * Constructs a basic exception during the handling of a task.
+     */
     public TaskHandlingException() {
     }
 
+    /**
+     * Constructs a basic exception during the handling of a task.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
     public TaskHandlingException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a basic exception during the handling of a task.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     * @param cause   the cause (which is saved for later retrieval by the
+     *                {@link #getCause()} method).  (A <tt>null</tt> value is
+     *                permitted, and indicates that the cause is nonexistent or
+     *                unknown.)
+     */
     public TaskHandlingException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Constructs a basic exception during the handling of a task.
+     *
+     * @param cause the cause (which is saved for later retrieval by the
+     *              {@link #getCause()} method).  (A <tt>null</tt> value is
+     *              permitted, and indicates that the cause is nonexistent or
+     *              unknown.)
+     */
     public TaskHandlingException(Throwable cause) {
         super(cause);
     }
