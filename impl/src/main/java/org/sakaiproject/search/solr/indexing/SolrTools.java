6058d417f04bdeb0989eada89e3f93e72e09b1c9
Colin Hebert <colin.hebert@it.ox.ac.uk>
2013-03-28 13:40:39 +0000

    Allow tika to generate documents with 10M characters
    The default limit of characters parsed by Tika is 100k characters,
    this could result in some large documents (thesis, etc.) being
    truncated.
    The limit is now 10M and should be perfect for every kind of document.
    A limit has to be set to avoid PDF bombs etc.

--- impl/src/main/java/org/sakaiproject/search/solr/indexing/SolrTools.java
+++ impl/src/main/java/org/sakaiproject/search/solr/indexing/SolrTools.java
@@ -38,6 +38,10 @@ public class SolrTools {
     private static final String PROPERTY_PREFIX = "property_";
     private static final String UPREFIX = PROPERTY_PREFIX + "tika_";
     private static final Logger logger = LoggerFactory.getLogger(SolrTools.class);
+    /**
+     * Maximum number of characters retreived in a document parsed by Tika
+     */
+    private static final int MAX_STRING_LENGTH = 10000000;
     private SiteService siteService;
     private SearchIndexBuilder searchIndexBuilder;
     private ContentProducerFactory contentProducerFactory;
@@ -49,6 +53,7 @@ public class SolrTools {
      */
     public void init() {
         tika = new Tika();
+        tika.setMaxStringLength(MAX_STRING_LENGTH);
     }
 
     /**
